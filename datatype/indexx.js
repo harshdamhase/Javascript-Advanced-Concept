28cc335e51de30701b3940aa89284261151aef4d
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-21 03:53:06 -0400

    datatypes

---
+++ datatype/indexx.js
@@ -0,0 +1,14 @@
+let num = 10;
+let name = "Harshdaa";
+let state = null;
+let isBoolean = true;
+
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(state));
+console.log(typeof(isBoolean));
+
+
+// null = standalone CSSMathValue. it is empty
+// undefined = value not assigned to variable
+// symbol = unique value
