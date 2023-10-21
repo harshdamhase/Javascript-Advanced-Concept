c89aff7d5995391e00de2982a476761183792315
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-21 03:35:27 -0400

    variables eg added

---
+++ variables/index.js
@@ -0,0 +1,6 @@
+const accno = 10;
+let accName = savingAccount;
+var accHolderName = "harshda";
+console.log('Accno'+accno);
+console.log('accName'+accName);
+console.log('accHolderName'+accHolderName);
